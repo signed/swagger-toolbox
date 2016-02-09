b4b7ae5970609413df777ad2cc3c84f2aa0ebc44
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-02-09 20:34:55 +0100

    Prepare for next test

--- src/test/java/com/github/signed/swagger/merge/SwaggerMerger_Test.java
+++ src/test/java/com/github/signed/swagger/merge/SwaggerMerger_Test.java
@@ -2,10 +2,11 @@ package com.github.signed.swagger.merge;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import com.github.signed.swagger.essentials.SwaggerMother;
 import com.github.signed.swagger.essentials.SwaggerBuilder;
+import com.github.signed.swagger.essentials.SwaggerMother;
 
 public class SwaggerMerger_Test {
 
@@ -13,12 +14,19 @@ public class SwaggerMerger_Test {
     private final SwaggerBuilder second = SwaggerMother.emptyApiDefinition();
     private final SwaggerMerger swaggerMerger = new SwaggerMerger();
 
-    @Test
-    public void same_path_but_different_operations() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         first.withPath("/{variable}/constant").withPost();
         second.withPath("/{argument}/constant").withOption();
-        MergeResult result = swaggerMerger.merge(first.build(), second.build());
-        assertThat("merging the same path with different operations should not be successful", !result.success());
+    }
+
+    @Test
+    public void same_path_but_different_operations_results_in_merge_conflict() throws Exception {
+        assertThat("merging the same path with different operations should not be successful", !mergeResult().success());
+    }
+
+    private MergeResult mergeResult() {
+        return swaggerMerger.merge(first.build(), second.build());
     }
 
 }
