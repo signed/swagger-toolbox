c5e1700fbda146568b8d33a3d1d6e0d1bbca22d6
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-02-09 20:57:35 +0100

    Remove duplication

--- src/main/java/com/github/signed/swagger/trim/PathContainedInBooth.java
+++ src/main/java/com/github/signed/swagger/trim/PathContainedInBooth.java
@@ -3,12 +3,15 @@ package com.github.signed.swagger.trim;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import com.github.signed.swagger.essentials.SwaggerStreams;
+
 import io.swagger.models.Swagger;
 
 public class PathContainedInBooth {
+    private final SwaggerStreams swaggerStreams = new SwaggerStreams();
 
     public Predicate<String> pathContainedInBooth(Swagger two){
-        return exposedPath -> two.getPaths().keySet().stream()
+        return exposedPath -> swaggerStreams.pathStream(two)
                 .map(this::unifyUrlTemplateVariableNames)
                 .collect(Collectors.toList())
                 .contains(unifyUrlTemplateVariableNames(exposedPath));
