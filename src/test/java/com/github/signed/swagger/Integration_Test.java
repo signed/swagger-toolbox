ef65bff2ca3962ce58a4b61b1213aee28ee344f2
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-03-04 22:25:20 +0100

    add asserts to integration tests

--- src/test/java/com/github/signed/swagger/Integration_Test.java
+++ src/test/java/com/github/signed/swagger/Integration_Test.java
@@ -1,47 +1,48 @@
 package com.github.signed.swagger;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import com.github.signed.swagger.merge.SwaggerMerge;
+import com.github.signed.swagger.reduce.SwaggerReduce;
+import com.github.signed.swagger.trim.SwaggerTrim;
+import io.swagger.models.Path;
+import io.swagger.models.Swagger;
+import io.swagger.parser.SwaggerParser;
+import io.swagger.util.Json;
+import io.swagger.util.Yaml;
+import org.junit.Test;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.junit.Test;
-
-import com.github.signed.swagger.merge.SwaggerMerge;
-import com.github.signed.swagger.reduce.SwaggerReduce;
-import com.github.signed.swagger.trim.SwaggerTrim;
-
-import io.swagger.models.Path;
-import io.swagger.models.Swagger;
-import io.swagger.parser.SwaggerParser;
-import io.swagger.util.Json;
-import io.swagger.util.Yaml;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Integration_Test {
 
+    public static final String MarkerTag = "public";
     private final String first = TestFiles.Json.petstoreExample();
     private final String second = TestFiles.Json.petstoreExample();
     private final SwaggerParser parser = new SwaggerParser();
-    private final SwaggerReduce reduce = new SwaggerReduce("public");
+    private final SwaggerReduce reduce = new SwaggerReduce(MarkerTag);
     private final SwaggerTrim trim = new SwaggerTrim();
     private final SwaggerMerge merge = new SwaggerMerge();
 
     @Test
     public void just_reduce() throws Exception {
         Swagger petShop = parser.read(first);
-        petShop.getPaths().values().stream().map(Path::getOperations).flatMap(Collection::stream).forEach(a -> a.tag("public"));
+        petShop.getPaths().values().stream().map(Path::getOperations).flatMap(Collection::stream).forEach(a -> a.tag(MarkerTag));
         reduce.reduce(petShop);
 
         Yaml.prettyPrint(petShop);
+        assertThat(petShop, notNullValue());
     }
 
     @Test
     public void reduce_trim() throws Exception {
         Swagger petShop = parser.read(first);
-        petShop.getPaths().values().stream().map(Path::getOperations).flatMap(Collection::stream).forEach(a -> a.tag("public"));
+        petShop.getPaths().values().stream().map(Path::getOperations).flatMap(Collection::stream).forEach(a -> a.tag(MarkerTag));
         reduce.reduce(petShop);
 
         Yaml.prettyPrint(petShop);
@@ -49,23 +50,19 @@ public class Integration_Test {
         trim.trim(petShop);
 
         Yaml.prettyPrint(petShop);
+        assertThat(petShop, notNullValue());
     }
 
     @Test
     public void reduce_trim_merge() throws Exception {
         Swagger _1st = parser.read(first);
-        _1st.getPaths().values().stream().map(Path::getOperations).flatMap(Collection::stream).forEach(a -> a.tag("public"));
+        _1st.getPaths().values().stream().map(Path::getOperations).flatMap(Collection::stream).forEach(a -> a.tag(MarkerTag));
         Swagger _2nd = parser.read(second);
         List<Swagger> collect = Stream.of(_1st, _2nd).map(reduce::reduce).map(trim::trim).collect(Collectors.toList());
         Swagger result = this.merge.merge(collect.get(0), collect.get(1)).swagger();
         Yaml.prettyPrint(_1st);
         Json.prettyPrint(result);
-    }
-
-    @Test
-    public void load_map_definition_found_on_the_web() throws Exception {
-        Swagger mapDefinition = parser.read(TestFiles.Yaml.mapDefinition());
-        System.out.println(mapDefinition);
+        assertThat(result, notNullValue());
     }
 
     @Test
