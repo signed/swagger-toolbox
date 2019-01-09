d5111ca1c4450603d32697706ac6c999471a1258
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-01-09 23:54:40 +0100

    migrate parameterized test to junit 5

--- src/test/java/com/github/signed/swagger/essentials/Properties_ThatDoNotContainDefinitionReferencesTest.java
+++ src/test/java/com/github/signed/swagger/essentials/Properties_ThatDoNotContainDefinitionReferencesTest.java
@@ -6,9 +6,6 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.hamcrest.Matchers;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import io.swagger.models.properties.DateProperty;
 import io.swagger.models.properties.DateTimeProperty;
@@ -17,34 +14,29 @@ import io.swagger.models.properties.IntegerProperty;
 import io.swagger.models.properties.PasswordProperty;
 import io.swagger.models.properties.Property;
 import io.swagger.models.properties.StringProperty;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-@RunWith(Parameterized.class)
-public class Properties_ThatDoNotContainDefinitionReferencesTest {
+class Properties_ThatDoNotContainDefinitionReferencesTest {
 
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
+    static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {new PasswordProperty()}
-                , {new StringProperty()}
-                , {new EmailProperty()}
-                , {new DateTimeProperty()}
-                , {new IntegerProperty()}
-                , {new DateProperty()}
-                , {new DateProperty()}
+            {new PasswordProperty()}
+            , {new StringProperty()}
+            , {new EmailProperty()}
+            , {new DateTimeProperty()}
+            , {new IntegerProperty()}
+            , {new DateProperty()}
+            , {new DateProperty()}
 
         });
     }
 
     private final Properties properties = new Properties();
-    private final Property property;
 
-
-    public Properties_ThatDoNotContainDefinitionReferencesTest(Property property) {
-        this.property = property;
-    }
-
-    @Test
-    public void test() {
+    @ParameterizedTest
+    @MethodSource("data")
+    void test(Property property) {
         assertThat(properties.definitionReferencesIn(property), Matchers.empty());
     }
 
